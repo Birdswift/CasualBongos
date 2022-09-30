d87f8da08194b85036d802d8381716fc0720bfb3
Birdswift <112816261+Birdswift@users.noreply.github.com>
2022-09-30 10:25:27 +0300

    Update Inches.cpp

--- Inches.cpp
+++ Inches.cpp
@@ -3,7 +3,8 @@
 int main()
 {
     double length_inch, length_meters, milimeters, meters, sentimeters;
-
+  std::cout<<"Task2"<<std::endl;
+  std::cout<<"How many inches?"<<std::endl;
   std::cin >> length_inch;
     length_meters = 25.4 * length_inch; //перевожу в метры
 
@@ -11,7 +12,7 @@ int main()
     sentimeters = int((length_meters - meters * 1000) / 10); //нахожу количество целых сантиметров
 
     milimeters = (length_meters - meters * 1000 - sentimeters * 10); //нахожу количество нецелых миллиметров, с точностью до десятых
-
+    std::cout<<length_inch<< " inches are ";
    std::cout << meters<<" meters " <<sentimeters<< " sentimeters "<< milimeters<< " milimeters ";
 
 
