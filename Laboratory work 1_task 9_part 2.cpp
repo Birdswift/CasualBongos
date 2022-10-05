e8d3e51baf2506ed37f7f8ebe4d39bde0640f274
Birdswift <112816261+Birdswift@users.noreply.github.com>
2022-10-05 08:52:00 +0300

    Add files via upload

---
+++ Laboratory work 1_task 9_part 2.cpp
@@ -0,0 +1,17 @@
+﻿//Является ли треугольник тупоугольным? Task_9 part_2
+#include <iostream>
+int main()
+{
+    int a, b, c, i = 0;
+    setlocale(LC_ALL, "rus");
+    std::cout << "Введите стороны треугольника" << std::endl;
+    std::cin >> a >> b >> c;
+    if (((c ^ 2) < (a ^ 2 + b ^ 2)) || ((a ^ 2) < (c ^ 2 + b ^ 2)) || ((b ^ 2) < (a ^ 2 + c ^ 2)))
+    {
+     std::cout << "Треугольник является тупоугольным" <<std::endl;
+    }
+    else {
+     std::cout << "Треугольник не является тупоугольным" <<std::endl;
+    }
+    return 0;
+}
